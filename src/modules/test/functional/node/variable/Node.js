f3bc2ea2352ee9f0051296a1c68911897d032164
四格 <michaelluthor@163.com>
2022-11-25 12:26:03 +0800

    add functional test flow node "Variable"

---
+++ src/modules/test/functional/node/variable/Node.js
@@ -0,0 +1,92 @@
+import NodeBase from "../NodeBase";
+/**
+ * functional test flow node "variable"
+ * the "variable" node read value from input, and execute the 
+ * expression and the send the result to output
+ */
+export default class Node extends NodeBase {
+    /**
+     * @constructor
+     */
+    constructor() {
+        super();
+        debugger
+        this.title = this.$t('name');
+        this.inputName = this.$t('input');
+        this.outputName = this.$t('output');
+
+        // setup input and output
+        this.addInput(this.inputName, 'string');
+        this.addOutput(this.outputName, 'string');
+
+        // setup widget button for variable editting.
+        this.onBtnSettingClicked = () => {};
+        this.btnDirectiveSelect = this.addWidget(
+            'button',
+            this.$t('setting'),
+            null,
+            () => this.onBtnSettingClicked(this)
+        );
+
+        // value viewer
+        this.valueWidget = this.addWidget('text',this.$t('value'),'',()=>{},{disabled:true});
+        this.valueWidget.disabled = true;
+    }
+
+    /**
+     * get type name of this node
+     * @returns {String}
+     */
+    getTypeName() {
+        return 'Variable';
+    }
+
+    /**
+     * get init options
+     * @returns {Object}
+     */
+    getInitOptions() {
+        return {
+            name : this.$t('unnamed'),
+            defaultValue : undefined,
+            expression : '',
+        };
+    }
+
+    /**
+     * handler on option updated
+     */
+    onOptionUpdate() {
+        this.title = this.$t('name') + ' : ' + this.options.name;
+        this.valueWidget.value = '';
+        this.refresh();
+    }
+
+    /**
+     * execute the node
+     */
+    async executeAction () {
+        this.valueWidget.value = '';
+        this.refresh();
+
+        let value = this.getInputDataByName(this.inputName);
+        if ( undefined === value ) {
+            value = this.options.defaultValue;
+        }
+
+        if ( 0 != this.options.expression.trim().length ) {
+            try {
+                let expr = this.options.expression.replaceAll('{{value}}', value);
+                let exprFunc = Function(`return ${expr};`);
+                value = exprFunc.call({});
+            } catch ( e ) {
+                this.alert('error', this.$t('expressionExecFailed', e.message));
+                return ;
+            }
+        }
+
+        this.valueWidget.value = value;
+        this.setOutputDataByName(this.outputName, value);
+        this.refresh();
+    }
+}
---
+++ src/modules/test/functional/node/variable/i18n/zh.lang.js
@@ -0,0 +1,13 @@
+module.exports = {
+    name : '变量',
+    setting : '配置',
+    input : '输入',
+    output : '输出',
+    value : '值',
+    unnamed : '未命名',
+    expressionExecFailed : '表达式执行失败：{0}',
+    settingTitle : '变量配置',
+    varName : '变量名',
+    inputDefaultValue : '输入默认值',
+    expression : '表达式',
+};
