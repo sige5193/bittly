480b97f196abce7e908968228bdcc99677d42f79
四格 <michaelluthor@163.com>
2023-01-24 13:41:42 +0800

    add script to build webapp

---
+++ scripts/build-webapp.ps1
@@ -0,0 +1,26 @@
+# 
+# build production packages
+#
+
+# start workflow
+Write-Host ""
+Write-Host "Start workflow ..."
+gh workflow run "Build Web App" --repo sige5193/bittly
+if ( $false -eq $? ) {
+   return ;
+}
+
+# delay 3 seconds
+Start-Sleep -s 3
+
+# watch workflow job
+$jobs = gh run list --repo sige5193/bittly --user sige5193 --limit 1 --json databaseId
+$config = $jobs | ConvertFrom-Json
+$jobId = $config[0].databaseId
+gh run watch $jobId --repo sige5193/bittly --exit-status
+if ( $false -eq $? ) {
+   return ;
+}
+
+# Done
+Write-Host ""
