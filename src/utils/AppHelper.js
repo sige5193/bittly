aed48a7cc6eec57bd6272bd53f7bc88ea7a42c94
四格 <michaelluthor@163.com>
2023-01-24 21:46:59 +0800

    get nav app language from env

--- src/utils/AppHelper.js
+++ src/utils/AppHelper.js
@@ -1,5 +1,5 @@
-import BittlyMainClient from "./BittlyMainClient";
 import MdbRuntimeVariable from '../models/MdbRuntimeVariable.js'
+import Environment from "../environments/Environment.js";
 export default class AppHelper {
     /**
      * get application language code
@@ -8,8 +8,9 @@ export default class AppHelper {
     static async langCodeGet() {
         let lang = await MdbRuntimeVariable.getVarValue('app_language', null);
         if ( null == lang ) {
+            let env = Environment.getEnv();
             let countryLangMap = {CN:'zh'};
-            let countryCode = await BittlyMainClient.systemLocaleCountryCodeGet();
+            let countryCode = await env.langCodeGet();
             lang = countryLangMap[countryCode] || 'zh';
         }
         return lang;
