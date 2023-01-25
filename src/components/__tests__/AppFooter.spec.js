2c4c7209307ee44cd989ec81a32541b8dcaa1a22
四格 <michaelluthor@163.com>
2023-01-25 19:06:14 +0800

    update test case

--- src/components/__tests__/AppFooter.spec.js
+++ src/components/__tests__/AppFooter.spec.js
@@ -2,24 +2,11 @@ import UnitTester from '../../utils/test/UnitTester.js';
 import AppFooter from '../AppFooter.vue'
 describe('@/components/AppFooter.vue', () => {
     it('basic', async ( ) => {
-        let tester = new UnitTester({
-            mockStoreGetters : {
-                communicators : {
-                    com01 : { getDeviceType : () => 'modbus', },
-                    com02 : { getDeviceType : () => 'network', },
-                },
-            },
-        });
+        let tester = new UnitTester();
         await tester.setup();
         await tester.mount(AppFooter, true);
-        await tester.eventBusEmit('communicator-online');
-        expect(tester.count({ref:'communicators'})).toBe(2);
 
-        tester.wrapper.vm.$store.getters.communicators = {};
-        await tester.eventBusEmit('communicator-offline');
-        expect(tester.count({ref:'communicators'})).toBe(0);
-
-        window.shell = {openExternal : jest.fn()};
+        window.shell.openExternal = jest.fn();
         await tester.click({ref:'iconQQ'});
         expect(window.shell.openExternal).toBeCalledTimes(1);
     });
