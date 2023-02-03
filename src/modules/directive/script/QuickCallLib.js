7e5c0e27cd2c8fb537d1eaa1a8e2acebecc071f9
四格 <michaelluthor@163.com>
2023-02-03 20:48:22 +0800

    [done #27] add more crc functions to quick call

--- src/modules/directive/script/QuickCallLib.js
+++ src/modules/directive/script/QuickCallLib.js
@@ -13,6 +13,16 @@ export default class QuickCallLib {
             {func:'checksum8',value:'{{@checksum8(items, ...)}}',text:$t('directive.quickCallChecksum8')},
             {func:'bcc',value:'{{@bcc(items, ...)}}',text:$t('directive.quickCallBcc')},
             {func:'fill',value:'{{@fill(content,count)}}',text:$t('directive.quickCallFill')},
+            {func:'crc1',value:'{{@crc1($1,$2,$3,$4)}}',text:$t('directive.quickCallCrc1')},
+            {func:'crc8',value:'{{@crc8($1,$2,$3,$4)}}',text:$t('directive.quickCallCrc8')},
+            {func:'crc81wire',value:'{{@crc81wire($1,$2,$3,$4)}}',text:$t('directive.quickCallCrc81wire')},
+            {func:'crc16',value:'{{@crc16($1,$2,$3,$4)}}',text:$t('directive.quickCallCrc16')},
+            {func:'crc16ccitt',value:'{{@crc16ccitt($1,$2,$3,$4)}}',text:$t('directive.quickCallCrc16ccitt')},
+            {func:'crc16xmodem',value:'{{@crc16xmodem($1,$2,$3,$4)}}',text:$t('directive.quickCallCrc16xmodem')},
+            {func:'crc16kermit',value:'{{@crc16kermit($1,$2,$3,$4)}}',text:$t('directive.quickCallCrc16kermit')},
+            {func:'crc24',value:'{{@crc24($1,$2,$3,$4)}}',text:$t('directive.quickCallCrc24')},
+            {func:'crc32',value:'{{@crc32($1,$2,$3,$4)}}',text:$t('directive.quickCallCrc32')},
+            {func:'crcjam',value:'{{@crcjam($1,$2,$3,$4)}}',text:$t('directive.quickCallCrcjam')},
         ];
     }
 
@@ -46,6 +56,106 @@ export default class QuickCallLib {
         resultView.setUint16(0, crc);
         let result = resultView.getUint16(0, true);
         return result.toString();
+    }
+
+    /**
+     * crc1
+     * @param {Array|Object|String} data 
+     * @returns {Number}
+     */
+    crc1( ... data ) {
+        let crc = this.scriptLib.crc('crc1', ... data);
+        return crc.toString();
+    }
+
+    /**
+     * crc8
+     * @param {Array|Object|String} data 
+     * @returns {Number}
+     */
+    crc8( ... data ) {
+        let crc = this.scriptLib.crc('crc8', ... data);
+        return crc.toString();
+    }
+
+    /**
+     * crc81wire
+     * @param {Array|Object|String} data 
+     * @returns {Number}
+     */
+    crc81wire( ... data ) {
+        let crc = this.scriptLib.crc('crc81wire', ... data);
+        return crc.toString();
+    }
+
+    /**
+     * crc16
+     * @param {Array|Object|String} data 
+     * @returns {Number}
+     */
+    crc16( ... data ) {
+        let crc = this.scriptLib.crc('crc16', ... data);
+        return crc.toString();
+    }
+
+    /**
+     * crc16ccitt
+     * @param {Array|Object|String} data 
+     * @returns {Number}
+     */
+    crc16ccitt( ... data ) {
+        let crc = this.scriptLib.crc('crc16ccitt', ... data);
+        return crc.toString();
+    }
+
+    /**
+     * crc16xmodem
+     * @param {Array|Object|String} data 
+     * @returns {Number}
+     */
+    crc16xmodem( ... data ) {
+        let crc = this.scriptLib.crc('crc16xmodem', ... data);
+        return crc.toString();
+    }
+
+    /**
+     * crc16kermit
+     * @param {Array|Object|String} data 
+     * @returns {Number}
+     */
+    crc16kermit( ... data ) {
+        let crc = this.scriptLib.crc('crc16kermit', ... data);
+        return crc.toString();
+    }
+
+    /**
+     * crc24
+     * @param {Array|Object|String} data 
+     * @returns {Number}
+     */
+    crc24( ... data ) {
+        let crc = this.scriptLib.crc('crc24', ... data);
+        return crc.toString();
+    }
+
+    /**
+     * crc32
+     * @param {Array|Object|String} data 
+     * @returns {Number}
+     */
+    crc32( ... data ) {
+        let crc = this.scriptLib.crc('crc32', ... data);
+        return crc.toString();
+    }
+
+    /**
+     * crcjam
+     * @param {Array|Object|String} data 
+     * @returns {Number}
+     */
+    crcjam( ... data ) {
+        let crc = this.scriptLib.crc('crcjam', ... data);
+        return crc.toString();
     }
 
     /**
